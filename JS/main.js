c2079e498f24ea9a78b3b0e638b3b2a17a0ec87e
Tim Knippers <timmyknippers@hotmail.com>
2016-08-04 15:12:13 +0200

    Initial version

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+﻿// starting the script on page load
+$(document).ready(function () {
+    $("#draggable").draggable();
+    $("#droppable").droppable({
+        drop: function (event, ui) {
+            $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+        }
+    });
+});
+
