f76131eb4a4ace3930e2ff0fe056ad06082f27f5
Tim Knippers <timmyknippers@hotmail.com>
2016-08-04 15:19:07 +0200

    Comments added

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,11 @@
-﻿// starting the script on page load
+﻿/* Start script on page load */
 $(document).ready(function () {
+    // Create draggable element
     $("#draggable").draggable();
+    // Create droppable elment
     $("#droppable").droppable({
         drop: function (event, ui) {
+            // At dropping, add Class and show label
             $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
         }
     });
